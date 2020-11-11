b12f6b8ee9e6e8427a123dfaf227044497f0a13a
Daniel.Yu <adam741963@gmail.com>
2020-11-10 21:24:32 -0800

    database and api router 0.1

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// mongodb connection
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project3',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+const connection = mongoose.connection;
+connection.once('open', function() {
+  console.log('MongoDE connection successed');
+});
+// routes
+require('./router/apiRoute')(app);
 
 // Send every request to the React app
 // Define any API routes before this runs
