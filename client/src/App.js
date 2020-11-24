d11cdee6eb5833e5c2a86d6903bb023d6a346bd9
faranak Sabet <faranaksabet@yahoo.com>
2020-11-24 09:38:50 -0800

    Changes to app file

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,11 @@ import React from 'react';
 //import Register from './components/Forms/Register';
 import Login from './components/Forms/Login.js';
 //import Start from './Start/Start';
-//import AgePicker from './components/Category/AgePicker';
-//import CategoryPicker from './components/Category/CategoryPicker';
-//import MediaPicker from './components/Category/MediaPicker';
+import AgePicker from './components/Category/AgePicker';
+import CategoryPicker from './components/Category/CategoryPicker';
+import MediaPicker from './components/Category/MediaPicker';
 import Navbar from './components/Navbar/Navbar';
-import Login from './Login/Login';
+//import Login from './Login/Login';
 
 export default class App extends React.Component {
   constructor(props) {
